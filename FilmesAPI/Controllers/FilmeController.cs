24e010658f0c3cf1da7a352fae66b46cb86c92b1
Gabriele Araújo <gabriele.araujo@aluno.ufop.edu.br>
2022-07-07 16:07:19 -0300

    Initial commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,50 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            /*foreach(Filme filme in filmes){
+                if(filme.Id == id)
+                {
+                    return filme;
+                }
+            }
+            return null;*/
+
+            //return filmes.FirstOrDefault(filme => filme.Id == id);
+
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
