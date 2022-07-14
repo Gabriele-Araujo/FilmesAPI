1d747a3f3da00c3facdd4a510d4ca938e6b129ac
Gabriele Araújo <gabriele.araujo@aluno.ufop.edu.br>
2022-07-14 15:08:32 -0300

    Conexão banco

--- FilmesApi/Startup.cs
+++ FilmesApi/Startup.cs
@@ -34,6 +34,10 @@ namespace FilmesAPI
             services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseMySQL(Configuration.GetConnectionString("CinemaConnection")));
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
