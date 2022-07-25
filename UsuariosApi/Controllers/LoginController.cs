266757195ce541eb0adb314536638b1aa14bae33
Gabriele Araújo <gabriele.araujo@aluno.ufop.edu.br>
2022-07-25 17:07:29 -0300

    Efetuando o login

---
+++ UsuariosApi/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosApi.Data.Dtos.Usuario;
+using UsuariosApi.Data.Requests;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers
+{
+    public class LoginController : ControllerBase
+    {
+        private LoginService _loginService;
+
+        public LoginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult LogaUsuario(LoginRequest request)
+        {
+            Result resultado = _loginService.LogaUsuario(request);
+            if (resultado.IsFailed) return Unauthorized();
+            return Ok();
+        }
+    }
+}
--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UsuariosApi.Controllers;
 using UsuariosApi.Data;
 using UsuariosApi.Services;
 
@@ -31,17 +32,16 @@ namespace UsuariosApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<UserDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("UsuarioConnection")));
+            services.AddDbContext<UserDbContext>(options => 
+                options.UseMySQL(Configuration.GetConnectionString("UsuarioConnection"))
+            );
             services
                 .AddIdentity<IdentityUser<int>, IdentityRole<int>>()
                 .AddEntityFrameworkStores<UserDbContext>();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UsuariosApi", Version = "v1" });
-            });
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<CadastroService, CadastroService>();
+            services.AddScoped<LoginService, LoginService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,8 +50,6 @@ namespace UsuariosApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UsuariosApi v1"));
             }
 
             app.UseHttpsRedirection();
